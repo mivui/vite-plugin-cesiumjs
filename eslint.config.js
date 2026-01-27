318c4800c5af571fe3260a6ffbc92494b513562f
mivui <49021590+mivui@users.noreply.github.com>
2026-01-27 21:40:24 +0800

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+import { globalIgnores } from 'eslint/config';
 import { defineConfig } from 'typescript-eslint-standard';
 
-export default defineConfig();
+export default defineConfig({
+  extends: [globalIgnores(['dist'])],
+});
